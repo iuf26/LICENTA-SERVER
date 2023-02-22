6eaf3898286e74431394e47009b28cc295cc7fc2
Iulia26 <ifilimon26@gmail.com>
2023-02-22 20:38:54 +0200

    added response helper

--- src/server/routes/user.route.js
+++ src/server/routes/user.route.js
@@ -2,7 +2,6 @@ import express from "express";
 import { validateTokenMiddleware } from "server/helpers/jwt.helper";
 import * as userController from "server/controllers/user.controller";
 import path from "path";
-import { renderFile } from "pug";
 
 const router = express.Router();
 
@@ -11,9 +10,6 @@ router.post("/register", userController.register);
 router.patch("/logout", validateTokenMiddleware, userController.logout);
 router.get("/verify/:userId/:otp", userController.verify);
 router.get("/verified", (req, res) => {
-  const html = renderFile(path.join(__dirname, "../../views/verified.pug"), {
-    redirectPage: process.env.LOGIN_PAGE_HOST,
-  });
   res.render(path.join(__dirname, "../../views/verified.pug"), {
     redirectPage: `${process.env.LOGIN_PAGE_HOST}`,
   });
